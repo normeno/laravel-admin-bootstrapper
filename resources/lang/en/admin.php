656f8dfbbe23499f2b9335d9b3c8629c58019560
Nicolas Ormeno <ni.ormeno@gmail.com>
2018-03-11 12:37:39 -0300

    Add more keys

--- resources/lang/en/admin.php
+++ resources/lang/en/admin.php
@@ -4,8 +4,8 @@ return [
     'users' => 'User|Users',
     'list_of' => 'List of :name',
     'creation_of' => ':name\'s creation',
-    'edit' => 'Editar',
-    'delete' => 'Eliminar',
+    'edit' => 'Edit',
+    'delete' => 'Delete',
     'idenfier' => 'Identifier',
     'identifier_ph' => 'Eg. DNI, Passport',
     'avatar' => 'Avatar',
@@ -18,4 +18,8 @@ return [
     'created_at' => 'Creation date',
     'create' => 'Create',
     'massive_creation' => 'Massive Creation',
+    'sign_in_to_start' => 'enter your credentials to access',
+    'remember_me' => 'Remember Me',
+    'sign_in_login' => 'Sign In',
+    'forgot_password' => 'Forgot Passowrd'
 ];
--- resources/lang/es/admin.php
+++ resources/lang/es/admin.php
@@ -1,7 +1,24 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: normeno
- * Date: 21-01-18
- * Time: 15:59
- */
+return [
+    'users' => 'Usuario|Usuarios',
+    'list_of' => 'Listado de :name',
+    'creation_of' => 'Creación de :name',
+    'edit' => 'Editar',
+    'delete' => 'Eliminar',
+    'idenfier' => 'Identificador',
+    'identifier_ph' => 'Eg. DNI, Passport',
+    'avatar' => 'Avatar',
+    'name' => 'Nombre',
+    'username' => 'Nombre de Usuario',
+    'email' => 'Correo',
+    'password' => 'Contraseña',
+    'confirm_password' => 'Confirmar Contraseña',
+    'actions' => 'Acciones',
+    'created_at' => 'Fecha de Creación',
+    'create' => 'Create',
+    'massive_creation' => 'Creación Masiva',
+    'sign_in_to_start' => 'Ingresa tus credenciales para acceder',
+    'remember_me' => 'Recordarme',
+    'sign_in_login' => 'Ingresar',
+    'forgot_password' => 'He olvidado mi contraseña'
+];
