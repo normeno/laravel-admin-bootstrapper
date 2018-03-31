f1772a7488cc8799bb69b00173baff4bdf9454ee
Nicolas Ormeno <ni.ormeno@gmail.com>
2018-03-31 00:00:15 -0300

    Add fillable

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'extra_id', 'avatar','name', 'username', 'email', 'password',
     ];
 
     /**
