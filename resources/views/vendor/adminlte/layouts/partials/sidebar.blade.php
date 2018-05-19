61931d29056707227024fe05d4c54202b1bae244
Nicolas Ormeno <ni.ormeno@gmail.com>
2018-05-19 10:33:03 -0400

    Improvenments on sidebard

--- resources/views/vendor/adminlte/layouts/partials/sidebar.blade.php
+++ resources/views/vendor/adminlte/layouts/partials/sidebar.blade.php
@@ -1,50 +1,32 @@
 <!-- Left side column. contains the logo and sidebar -->
 <aside class="main-sidebar">
 
-    <!-- sidebar: style can be found in sidebar.less -->
     <section class="sidebar">
 
-        <!-- Sidebar user panel (optional) -->
-        @if (! Auth::guest())
-            <div class="user-panel">
-                <div class="pull-left image">
-                    <img src="#" class="img-circle" alt="User Image"/>
-                </div>
-                <div class="pull-left info">
-                    <p>{{ Auth::user()->name }}</p>
-                    <!-- Status -->
-                    <a href="#"><i class="fa fa-circle text-success"></i> {{ trans('adminlte_lang::message.online') }}
-                    </a>
-                </div>
+        <div class="user-panel">
+            <div class="pull-left image">
+                <img src="{{ Auth::user()->avatar }}" class="img-circle" alt="{{ Auth::user()->name }}"/>
             </div>
-    @endif
-
-    <!-- search form (Optional) -->
-        <form action="#" method="get" class="sidebar-form">
-            <div class="input-group">
-                <input type="text" name="q" class="form-control"
-                       placeholder="{{ trans('adminlte_lang::message.search') }}..."/>
-                <span class="input-group-btn">
-                <button type='submit' name='search' id='search-btn' class="btn btn-flat"><i
-                            class="fa fa-search"></i></button>
-              </span>
+            <div class="pull-left info">
+                <p>{{ Auth::user()->name }}</p>
+                <a href="#"><i class="fa fa-circle text-success"></i> {{ trans('adminlte_lang::message.online') }}
+                </a>
             </div>
-        </form>
-        <!-- /.search form -->
+        </div>
 
         <!-- Sidebar Menu -->
         <ul class="sidebar-menu" data-widget="tree">
             <li class="header">{{ trans('adminlte_lang::message.header') }}</li>
 
             <li class="active">
-                <a href="#">
+                <a href="{{ route('admin.dashboard.index') }}">
                     <i class='fa fa-dashboard'></i>
                     <span>{{ trans('admin.dashboard') }}</span>
                 </a>
             </li>
 
             <li class="treeview">
-                <a href="{{ route('admin.users.index') }}">
+                <a href="#">
                     <i class='fa fa-users'></i>
                     <span>{{ trans_choice('admin.users', 10) }}</span>
                     <i class="fa fa-angle-left pull-right"></i>
